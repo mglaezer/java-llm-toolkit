e5f8029ad263589d10ff8f38cd85b44f7cbeab13
mglaezer <maxim.glaezer@gmail.com>
2025-04-02 09:24:46 -0700

    allow Cue annotation in more places

--- src/main/java/org/llmtoolkit/core/annotations/Cue.java
+++ src/main/java/org/llmtoolkit/core/annotations/Cue.java
@@ -6,7 +6,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.TYPE})
+@Target({
+    ElementType.ANNOTATION_TYPE,
+    ElementType.CONSTRUCTOR,
+    ElementType.FIELD,
+    ElementType.LOCAL_VARIABLE,
+    ElementType.METHOD,
+    ElementType.MODULE,
+    ElementType.PACKAGE,
+    ElementType.PARAMETER,
+    ElementType.RECORD_COMPONENT,
+    ElementType.TYPE,
+    ElementType.TYPE_PARAMETER,
+    ElementType.TYPE_USE
+})
 public @interface Cue {
     String value();
 }
